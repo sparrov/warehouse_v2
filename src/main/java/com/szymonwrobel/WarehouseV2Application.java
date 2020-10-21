bbf3e38bf82c1ac8919c390a63505b48862786b1
Szymon <sparrov@gmail.com>
2020-10-21 15:50:38 +0200

    Initial commit

---
+++ src/main/java/com/szymonwrobel/WarehouseV2Application.java
@@ -0,0 +1,13 @@
+package com.szymonwrobel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/szymonwrobel/WarehouseV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymonwrobel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
